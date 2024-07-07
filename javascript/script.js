b422dc3aa081f5fd05adade5eb97e58365c43a2a
gyan-anchan <gyananchan29@gmail.com>
2024-07-07 14:58:57 +0530

    Delete javascript directory

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-function upDate(element){
-    document.getElementById("image").style.backgroundImage = `url('${element.src}')`;
-    document.getElementById("image").innerHTML = element.alt;
-    }
-
-function unDo(element){
-    document.getElementById("image").style.backgroundImage = "";
-    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
-}
